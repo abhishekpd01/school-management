511fe235a77c9452192ecd2f14d48afc5f105646
Abhishek Prasad <prasadabhishek550@gmail.com>
2025-03-07 20:24:01 +0530

    setup mysql db connection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const { checkConnection } = require('./config/connectToDb');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,4 +9,12 @@ app.get('/', (req, res) => {
     res.json({ message: ' This msg is form server' });
 })
 
-app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
+app.listen(PORT, async () => {
+    console.log(`Server is up and running on port ${PORT}`);
+    try {
+        await checkConnection();
+    } catch (error) {
+        console.log('Failed to initialize DB connection', error);
+        
+    }
+});
---
+++ config/connectToDb.js
@@ -0,0 +1,24 @@
+// connect to DB
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    connectionLimit: 50,
+    queueLimit: 25,
+    waitForConnections: true
+});
+
+const checkConnection = async () => {
+    try {
+        const connection = await pool.getConnection();
+        console.log('Connected to DB');
+        connection.release();
+    } catch (error) {
+        console.log('Error connecting to DB:', error);
+    }
+}
+
+module.exports = { pool, checkConnection };
